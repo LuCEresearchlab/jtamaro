d7a6034522a98397bd5ca147f7fce7584f060e95
Joey Bevilacqua <bevilj@usi.ch>
2024-02-20 11:12:13 +0100

    Add module-info to lib

---
+++ lib/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module jtamaro {
+  requires java.datatransfer;
+  requires java.desktop;
+  requires java.xml;
+
+  exports jtamaro.en;
+  exports jtamaro.en.data;
+  exports jtamaro.en.graphic;
+  exports jtamaro.en.io;
+  exports jtamaro.en.music;
+}
