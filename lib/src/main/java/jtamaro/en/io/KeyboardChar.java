3baf350ebdd1d37a825056448f47938f3bc39d38
Joey Bevilacqua <bevilj@usi.ch>
2024-05-17 13:55:08 +0200

    KeyboardChar: store keyChar rather than the event
    Creating a "mock" event requires creating a "JComponent", that initializes
    the swing ui stuffs, which means that we effectively initialize a swing
    "application" when running tests that make use of this class.
    
    This commit is similar to cbd73f4, which solved the same issue for
    the MouseButton class and e43060f, which did the same for the KeyboardKey
    class.

--- lib/src/main/java/jtamaro/en/io/KeyboardChar.java
+++ lib/src/main/java/jtamaro/en/io/KeyboardChar.java
@@ -1,24 +1,44 @@
 package jtamaro.en.io;
 
 import java.awt.event.KeyEvent;
-
-import javax.swing.JComponent;
+import java.util.Objects;
 
 
 public final class KeyboardChar {
 
-  private final KeyEvent event;
+  private final char keyChar;
 
   public KeyboardChar(final KeyEvent event) {
-    this.event = event;
+    this.keyChar = event.getKeyChar();
   }
 
-  public KeyboardChar(final char theChar) {
-    this.event = new KeyEvent(new JComponent() {}, 0, 0, 0, KeyEvent.VK_UNDEFINED, theChar);
+  public KeyboardChar(final char keyChar) {
+    this.keyChar = keyChar;
   }
 
   public char getChar() {
-    return event.getKeyChar();
+    return keyChar;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    } else if (o == null || getClass() != o.getClass()) {
+      return false;
+    } else {
+      final KeyboardChar that = (KeyboardChar) o;
+      return keyChar == that.keyChar;
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(keyChar);
+  }
+
+  @Override
+  public String toString() {
+    return "KeyboardChar[char=" + keyChar + "]";
+  }
 }
